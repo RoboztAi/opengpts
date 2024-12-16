7f1f462350c60a3cda3ceeba7663f8023a68e524
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2024-12-16 23:42:49 +0700

    Update Authorize

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,10 +12,13 @@ export default defineConfig({
     },
     proxy: {
       "^/(assistants|threads|ingest|runs)": {
-        target: "https://gpt-api-dev.0xai.io",
-        changeOrigin: true,
-        rewrite: (path) => path.replace("/____LANGSERVE_BASE_URL", ""),
-
+        target: "http://192.168.20.158:8000",
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace("/____LANGSERVE_BASE_URL", ""),
+        
+        headers: {
+          "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVGluZ1RyYWNlIFByb2R1Y3Rpb24iLCJ1c2VyX2lkIjoiNjc0M2ViNWU5ZTQxZWQ3OTYwNDlkNGRjIiwiYXBwX2lkIjoiNjc0M2Y0MGRmMGM0YzNiNzBjOTM1ZDZmIn0.MXQWk_EA8T3NjbU80tGlLkMcWQd_zAPeNixtz7LGoSs"
+        }
       },
     },
   },
